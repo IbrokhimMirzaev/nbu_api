8e6ee51a1966a8ca724bee923e0f2a290ffc1d2f
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2022-08-04 19:54:30 +0500

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:nbu_api/service/get_currency_service.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Currency API"),
+      ),
+      body: FutureBuilder(
+          future: GetCurrencyService.getCurrency(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.data == "no") {
+              return const Center(child: Text("No internet connection"));
+            } else if (snapshot.data == "userError") {
+              return const Center(child: Text("user"));
+            } else {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data[index]["cb_price"]),
+                  );
+                },
+                itemCount: snapshot.data.length,
+              );
+            }
+          }),
+    );
+  }
+}
---
+++ lib/service/get_currency_service.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+class GetCurrencyService {
+  static Future getCurrency() async {
+    try {
+      Response res = await Dio().get("https://nbu.uz/uz/exchange-rates/json/");
+      if (res.statusCode == HttpStatus.ok) {
+        return res.data;
+      } else {
+        return "userError";
+      }
+    } catch (e) {
+      if (e is SocketException){
+        return "no";
+      }
+      throw Exception(e);
+    }
+  }
+}
